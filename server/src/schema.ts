33f0013f8e49a1ee5afe1ef2547c9f2abc1bfc7e
Lam Tran <lamtran8401@gmail.com>
2023-12-06 10:37:12 +0700

    server updated

---
+++ server/src/schema.ts
@@ -0,0 +1,33 @@
+import gql from 'graphql-tag'
+
+const typeDefs = gql`
+  type Query {
+    "Query to get tracks array for the homepage grid"
+    tracksForHome: [Track!]!
+  }
+
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    "The track's title"
+    title: String!
+    "The track's main author"
+    author: Author!
+    thumbnail: String
+    "The track's approximate length to complete, in minutes"
+    length: Int
+    "The number of modules this track contains"
+    modulesCount: Int
+  }
+
+  "Author of a complete Track"
+  type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture url"
+    photo: String
+  }
+`
+
+export default typeDefs
