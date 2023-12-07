075bcd552e7192d8b43ad18209c8a5beb25a793a
Lam Tran <lamtran8401@gmail.com>
2023-12-07 17:48:49 +0700

    update codegen

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+import ReactDOM from 'react-dom'
+import Pages from './pages'
+import GlobalStyles from './styles'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
-);
+)
